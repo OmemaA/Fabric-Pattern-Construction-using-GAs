6259b1f3260a65bba968602b78631031c5b1c801
Omema Ahmed <ahmed.omema@hotmail.com>
2021-05-24 01:26:41 +0500

    (❁´◡`❁)

--- GA2.py
+++ GA2.py
@@ -1,56 +1,129 @@
+from types import TracebackType
 from Colors import ColorPalette
 from PIL import Image
 from PIL import ImageDraw
 import numpy as np
 from random import randint
-# import image_slicer
+from random import random
+from random import choice
 
 class Shapes:
     def __init__(self):
-        self.colors = ColorPalette("Reference Images/1.jpg", 10).createPalette()
+        self.colors = ColorPalette("Reference Images/5.jpg", 10).createPalette()
         self.block = Image.new('RGBA', (200,200))
-        # self.lines = int(randint(3,10))
-        self.lines = 4
-        self.polygons = [] 
-        limits = [(0,200), (0,0), (0,200), (200,200)] # x1, y1, x2, y2
+        # self.iters = int(randint(3,10))
+        self.iters = 1
+        self.lines = []
+        self.polygons = []
+        limits= (0,0), (200,0), (0,200), (200,200)
+        self.division(limits)
+
+        # Attempt to draw polygons with colors filled in
+        # print(m_fill)
+        ImageDraw.Draw(self.block).rectangle((0, 0, 200, 200), fill='pink')
+        for i in range(len(self.polygons)):
+            m_fill = choice(self.colors)
+            m_fill = tuple([int(x*255) for x in m_fill])
+            ImageDraw.Draw(self.block).polygon(self.polygons[i], fill=m_fill)
+            
+        # for i in range(len(self.lines)):
+        #     ImageDraw.Draw(self.block).line(self.lines[i], fill='black')
+        # self.block.show()
+        self.form_tile(np.array(self.block))
+    
+        
+    def generate_random_point(self, p1, p2):
+        u = random()
+        pt_x, pt_y= int((1-u)*p1[0] + u*p2[0]), int((1-u)*p1[1] + u*p2[1])
+        return (pt_x, pt_y)
+
+    def new_limits(self, limits, pt1, pt2, option, segment=None):
+        first_half, second_half = [], []
+        if option == 0:  # slant and vertical
+            first_half = [(limits[0]), (pt1), (limits[2]), (pt2)] # x1, y1, x2, y2
+            second_half = [(pt1), (limits[1]), (pt2), (limits[3])]
+        elif option == 1: # slant and horizontal
+            first_half = [(limits[0]), (limits[1]), (pt1), (pt2)]
+            second_half = [(pt1), (pt2), (limits[2]), (limits[3])]
+        elif option == 2:
+            if segment == "TL":
+                first_half = [(limits[0]), (pt1), (pt2)]
+                second_half = [(pt1), (limits[1]), (pt2), (limits[2]), (limits[3])]
+            elif segment == "TR":
+                first_half = [(limits[0]), (pt1), (limits[1]), (limits[2], (pt2))]
+                second_half = [(pt1), (limits[1]), (pt2)]
+            elif segment == "BL":
+                first_half = [(pt1), (limits[2]), (pt2)]
+                second_half = [(limits[0]), (limits[1]), (pt1), (pt2), (limits[3])]
+            elif segment == "BR":
+                first_half = [(limits[0]), (limits[1]), (limits[2]), (pt1), (pt2)]
+                second_half = [(pt1), (pt2), (limits[3])]
+
+        return first_half, second_half
+
+    def form_tile(self,chromosomes):
+        hor=np.flip(chromosomes,0)
+        vert=np.flip(chromosomes,1)
+        left=np.flip(hor,1)
+        a=np.hstack((hor,chromosomes))
+        b=np.hstack((left,vert))
+        array_tuple = (a,b)
+        f=np.vstack(array_tuple)
+        img = Image.fromarray(f)
+        img.show()
+
+
+    def generate_pattern(self):
+        limits = []
+
         self.division(0,0, limits)
         for i in range(len(self.polygons)):
             ImageDraw.Draw(self.block).line(self.polygons[i])
         self.block.show()
+        
 
+    def division(self, limits, iterx=0, itery=0):
 
-    def division(self, iterx, itery, limits):
-        print("iterx", iterx, "itery", itery)
-        if(iterx == self.lines):
-            print("HEREE")
+        # print("iterx", iterx, "itery", itery)
+        if(iterx == self.iters):
+            # print("HEREE")
             iterx = 0
             return
-        elif(itery == self.lines):
+        if(itery == self.iters):
             itery = 0
             return
+        pt1, pt2 = (0,0), (0,0)
+        option = randint(0,1)
+        segment = None
+        if option == 0: # top and bottom 
+            pt1, pt2 = self.generate_random_point(limits[0], limits[1]), self.generate_random_point(limits[2], limits[3])  
+        elif option == 1: # left and right
+            pt1, pt2 = self.generate_random_point(limits[0], limits[2]), self.generate_random_point(limits[1], limits[3])  
+        elif option == 2: # vertical and horizontal
+            v_side, h_side = randint(0,1), randint(0,1)
+            if v_side == 0: # top 
+                pt1 = self.generate_random_point(limits[0], limits[1])
+                segment = "T"
+            elif v_side == 1: # bottom
+                pt1 = self.generate_random_point(limits[2], limits[3])
+                segment = "B"
 
-        coords = []
-        pt1 = randint(limits[0][0], limits[0][1]), randint(limits[1][0], limits[1][1])
-        pt2 = randint(limits[2][0], limits[2][1]), randint(limits[3][0], limits[3][1])
-        # print("pt1", pt1)
-        # print("pt2", pt2)
+            if h_side == 0: # left
+                pt2 = self.generate_random_point(limits[0], limits[2])
+                segment += "L"
+            elif h_side == 1: # right
+                pt2 = self.generate_random_point(limits[1], limits[3])
+                segment += "R"
 
-        coords.append((pt1[0], pt1[1]))
-        coords.append((pt2[0], pt2[1]))
-            
-        self.polygons.append(coords)
+        self.lines.append([pt1, pt2])
 
-        new_limits_left = [(limits[0][0], pt1[0]), (limits[1][0], pt1[1]), (limits[2][0], pt2[0]), (limits[3][0], pt2[1])]
-        new_limits_right = [(pt1[0], limits[0][1]), (pt1[1], limits[1][1]), (pt2[0], limits[2][1]), (pt2[1], limits[3][1])]
-        # print("left",new_limits_left)
-        # print("right",new_limits_right)
+        new_limits = self.new_limits(limits, pt1, pt2, option, segment)
+        self.polygons.extend(new_limits)
+        # print(new_limits)
+        self.division(new_limits[0], iterx+1, itery)
+        self.division(new_limits[1], iterx, itery+1)
 
-        self.division(iterx+1, itery, new_limits_left)
-        self.division(iterx, itery+1, new_limits_right)
 
 s = Shapes()
 
-            
 
-# c = ColorPalette("Reference Images/1.jpg", 5)
-# c.displayPalette()
---
+++ ImageQuality.py
@@ -0,0 +1,91 @@
+# import cv2
+from PIL import Image
+import numpy as np
+
+class ImageQuality():
+    def __init__(self, img):
+        self.img = img 
+
+    def area_occupied(self):
+        img = self.img.convert('RGB')
+        n_img = np.array(img)
+        h, w = n_img.shape[:2]
+
+        # Arrange all pixels as RGB values and find unique rows to get colours
+        colours, counts = np.unique(n_img.reshape(-1,3), axis=0, return_counts=1)
+
+        # Iterate through unique colours
+        for index, colour in enumerate(colours):
+            count = counts[index]
+            proportion = (100 * count) / (h * w)
+            print(f"   Colour: {colour}, count: {count}, proportion: {proportion:.2f}%")
+
+    def dullness_score(self):
+        # convert image to HSV (hue, saturation, value)
+        img = self.img.convert('HSV')
+        n_img = np.array(img)
+
+        # Arrange all pixels as RGB values and find unique rows to get colours
+        colours, counts = np.unique(n_img.reshape(-1,3), axis=0, return_counts=1)
+        
+
+        # Iterate through unique colours
+        for index, colour in enumerate(colours):
+            count = counts[index]
+            proportion = (100 * count) / (h * w)
+            print(f"   Colour: {colour}, count: {count}, proportion: {proportion:.2f}%")
+
+    def color_hormonies(self):
+        # convert image to HSV (hue, saturation, value)
+        img = self.img.convert('HSV')
+        n_img = np.array(img)
+        # print(n_img)
+
+    def global_contrast(self):
+        # Convert image to grayscale to get lightness channel
+        grayscale = self.img.convert('L')
+
+        # compute global min and max intensity values
+        minimum = np.min(grayscale)
+        maximum = np.max(grayscale)
+
+        # compute contrast
+        avg_contrast = (maximum - minimum)/(maximum + minimum)
+
+        return avg_contrast
+
+# i = Image.open("Reference Images/.jpg")
+# # i = Image.new('RGBA', (200,200))
+# IQ = ImageQuality(i).dullness_score()
+
+
+
+# cv2.imshow("image",self.img)
+# cv2.waitKey(0)
+
+# LAB_image = cv2.cvtColor(np.array(self.img), cv2.COLOR_BGR2LAB)
+# # Lightness channel
+# L = cv2.split(LAB_image)[0] 
+
+# # compute global min and max intensity values
+# minimum = np.min(L)
+# maximum = np.max(L)
+
+# # compute contrast
+# avg_contrast = (maximum - minimum)/(maximum + minimum)
+
+
+
+# kernel = np.ones((5,5),np.uint8)
+# min = cv2.erode(L,kernel,iterations = 1)
+# max = cv2.dilate(L,kernel,iterations = 1)
+
+# # convert min and max to floats
+# min = min.astype(np.float64) 
+# max = max.astype(np.float64) 
+
+# # compute local contrast
+# contrast = (max-min)/(max+min)
+
+# # get average across whole image
+# average_contrast = 100*np.mean(contrast)
